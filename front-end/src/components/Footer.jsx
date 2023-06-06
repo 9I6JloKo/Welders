d76ba936139b93098eff7eba8ee18360cf60e72f
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-06-06 17:43:55 +0300

    Done with footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
                 </div>
                 <div className="column">
                     <h3>General contact</h3>
-                    <div className="contact-container">
+                    <div className="general-contact-container">
                         <p>Ida-Virumaa Kutsehariduskeskus</p>
                         <p>Tallinna maantee 13 Sillamäe 40202</p>
                         <p>Telefon: <a href="tel:+3723320381">+372 3320 381</a></p>
@@ -46,26 +46,30 @@ export default function Footer() {
                         </div>
                         <div className="contact">
                             <p>Õppesekretär Narva</p>
-                            <a href="tel:+3723569343">+372 356 9343</a>
-                            <a href="tel:+3725108560">+372 510 8560</a>
+                            <div>
+                                <a href="tel:+3723569343">+372 356 9343</a>
+                                <a href="tel:+3725108560">+372 510 8560</a>
+                            </div>
                         </div>
                         <div className="contact">
                             <p>Täiendusõppe kursused</p>
-                            <a href="tel:+37256880405 ">+372 5688 0405</a>
-                            <a href="tel:+3725261318">+372 526 1318</a>
+                            <div>
+                                <a href="tel:+37256880405 ">+372 5688 0405</a>
+                                <a href="tel:+3725261318">+372 526 1318</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <nav>
                 <ul>
-                    <li><a href="https://kutsehariduskeskus.ee/en">Homepage</a></li>
-                    <li><a href="https://kutsehariduskeskus.ee/en/sitemap">Sitemap</a></li>
-                    <li><a href="https://www.facebook.com/IVKHK">Facebook</a></li>
-                    <li><a href="https://www.instagram.com/idavirumaa_kutsehariduskeskus/">Instagram</a></li>
-                    <li><a href="https://www.youtube.com/channel/UC6TPd9ePy3n-vFQRgtbhx-w">Youtube</a></li>
-                    <li><a href="https://vk.com/ivkhk">VKontakte</a></li>
-                    <li><a href="https://www.youtube.com/channel/UCLxa63lO1rkF62tv8d7P4Cw/videos">Kooli TV</a></li>
+                    <li><a href="https://kutsehariduskeskus.ee/en" target="_blank" rel="noopener noreferrer">Homepage</a></li>
+                    <li><a href="https://kutsehariduskeskus.ee/en/sitemap" target="_blank" rel="noopener noreferrer">Sitemap</a></li>
+                    <li><a href="https://www.facebook.com/IVKHK" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                    <li><a href="https://www.instagram.com/idavirumaa_kutsehariduskeskus/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li><a href="https://www.youtube.com/channel/UC6TPd9ePy3n-vFQRgtbhx-w" target="_blank" rel="noopener noreferrer">Youtube</a></li>
+                    <li><a href="https://vk.com/ivkhk" target="_blank" rel="noopener noreferrer">VKontakte</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCLxa63lO1rkF62tv8d7P4Cw/videos" target="_blank" rel="noopener noreferrer">Kooli TV</a></li>
                 </ul>
                 <p>Copyright 2020 © Ida-Virumaa Kutsehariduskeskus</p>
             </nav>
