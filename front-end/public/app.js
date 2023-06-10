7be151886575f7b385834f59c0b73188884b3753
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-06-10 22:52:11 +0300

    Header media is done

--- front-end/public/app.js
+++ front-end/public/app.js
@@ -3,6 +3,17 @@ $(document).ready(function () {
     headerTransition();
     $(window).scroll(headerTransition);
     $(window).resize(headerTransition);
+
+    // Burger menu colors
+    $('.navbar-toggler').click(function () {
+        $('.header .navbar-nav').show();
+        $('.nav-link').addClass(['white-burger', 'text-white']);
+        if ($('.nav-link').hasClass('white-burger')) {
+            $('.nav-link-apply').hide();
+        } else {
+            $('.nav-link-apply').show();
+        }
+    });
 });
 
 function headerTransition() {
@@ -11,53 +22,82 @@ function headerTransition() {
         // Show header on main page
         $('.header').addClass('main');
         $('.header .navbar-nav').show();
-        if ($(window).width() < 700) {
+        if ($(window).width() <= 1000) {
             $('.header .navbar-brand').css('margin-left', '0');
-        } else if ($(window).width() < 1010) {
-            $('.header .navbar-brand').css('margin-left', '-20px');
-        } else if ($(window).width() < 1150) {
-            $('.header .navbar-brand').css('margin-left', '30px');
+            $('.header .navbar-brand').addClass('w-100');
         } else {
             $('.header .navbar-brand').css('margin-left', '70px');
+            $('.header .navbar-brand').removeClass('w-100');
         }
 
         if ($(window).scrollTop() >= ($('.main-page').height() - $('.header').height() + 180)) {
             // Change header to white color
             $('.header').addClass('default');
+            // dark nav links + logo
+            $('.header a, .header p').addClass('text-dark');
+            $('.header a, .header p').removeClass('text-white');
+            // dark burger menu
             $('.header').removeClass('navbar-dark');
             $('.header').addClass('navbar-light');
-            $('header .navbar-nav .btn-container a').css('color', '#000');
-            $('.header .logo-text').css('color', '#000');
+            // increase logo saturation
             $('.header .navbar-brand svg').css('filter', 'saturate(150%)');
         } else {
             // Change header to black color
             $('.header').removeClass('default');
+            // white nav links + logo
+            $('.header a, .header p').addClass('text-white');
+            $('.header a, .header p').removeClass('text-dark');
+            // white burger menu
             $('.header').removeClass('navbar-light');
             $('.header').addClass('navbar-dark');
-            $('.navbar-nav .btn-container a').removeAttr('style');
-            $('.header .logo-text').css('color', '#fff');
+            // decrease logo saturation
             $('.header .navbar-brand svg').removeAttr('style');
         }
     } else {
         // Hide header
         $('.header').removeClass('main');
         $('.header .navbar-nav').hide();
+        // white burger menu
+        $('.header').removeClass('navbar-light');
+        $('.header').addClass('navbar-dark');
+
         if ($(window).width() < 650) {
             $('.header .navbar-brand').css('margin-left', '0');
-        } if ($(window).width() < 1010) {
+        } else if ($(window).width() < 991) {
             $('.header .navbar-brand').css('margin-left', '-20px');
         } else {
             $('.header .navbar-brand').css('margin-left', '120px');
         }
     }
+    if ($(window).width() <= 425) {
+        $('.header .navbar-brand').css({
+            transform: 'scale(0.75)',
+            marginLeft: '-80px',
+        });
+        if ($(window).width() <= 325) {
+            $('.header .navbar-brand').css('margin-left', '-50px');
+            $('.header .navbar-brand .logo-text p').css({
+                fontSize: '24px',
+                height: '20px',
+            });
+            $('.header button').css({
+                right: '15px',
+                padding: 0
+            });
+        }
+    } else {
+        $('.header .navbar-brand').removeAttr('style');
+        $('.header .navbar-brand .logo-text p').removeAttr('style');
+        $('.header button').removeAttr('style');
+    }
 
     // Mouse hover effect on apply button
     $('nav .btn-container a').hover(function () {
         $(window).scrollTop() >= $('.main-page').height()
-            ? $(this).css('color', '#fff') : $(this).css('color', '#fff');
+            ? $(this).addClass('text-white') : $(this).addClass('text-white');
     });
     $('nav .btn-container a').mouseleave(function () {
         $(window).scrollTop() >= $('.main-page').height()
-            ? $(this).css('color', '#000') : $(this).css('color', '#fff');
+            ? $(this).addClass('text-dark') : $(this).addClass('text-white');
     });
 }
--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,32 +1,9 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../assets/logo.svg';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 export default function Header() {
     return (
-        // <header>
-        //     <div className="logo">
-        //         <img src={logo} alt="ivkhk" />
-        //         <div className="logo-text">
-        //             <p>IDA-VIRUMAA</p>
-        //             <p>KUTSEHARIDUSKESKUS</p>
-        //         </div>
-        //     </div>
-        //     <nav>
-        //         <ul>
-        //             <li><a href="#top">Home</a></li>
-        //             <li><a  href="#aboutUs">About Us</a></li>
-        //             <li><a href="/program">Program</a></li>
-        //             <li><a href="#gallery">Gallery</a></li>
-        //             <li><a href="/team">Team</a></li>
-        //             <li><a href="/faq">FAQ</a></li>
-        //         </ul>
-        //         <div className="btn-container">
-        //             <a href="/contacts">Apply</a>
-        //         </div>
-        //     </nav>
-        // </header>
-
         <Navbar className='header' collapseOnSelect expand="lg">
             <Navbar.Brand href="#home">
                 <Logo
@@ -47,7 +24,7 @@ export default function Header() {
                     <Nav.Link className='text-center' href="#gallery">Gallery</Nav.Link>
                     <Nav.Link className='text-center' href="#team">Team</Nav.Link>
                     <Nav.Link className='text-center' href="#faq">FAQ</Nav.Link>
-                    <div className="btn-container">
+                    <div className="btn-container nav-link-apply">
                         <a href="#contacts">Apply</a>
                     </div>
                 </Nav>
